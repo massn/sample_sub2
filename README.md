0fe69055e0da389d4b28257e4db44495d1a3e641
Massn <massn23579@gmail.com>
2015-04-21 01:21:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sample_sub2
+Just to check the deps-managing of rebar
---
+++ src/sample_sub2_app.erl
@@ -0,0 +1,16 @@
+-module(sample_sub2_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    sample_sub2_sup:start_link().
+
+stop(_State) ->
+    ok.
